f679d00bcf8e6a88c9c5996f469d213408d3cac6
gdemirev01 <gdemirev78@gmail.com>
2019-08-12 19:40:01 +0300

    gains field added

---
+++ Ombudsman-project/Assets/GainsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GainsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject.Find("popularity").GetComponent<Text>().text = Profile.gains.popularity + "";
+        GameObject.Find("trust").GetComponent<Text>().text = Profile.gains.trust + "";
+        GameObject.Find("energy").GetComponent<Text>().text = Profile.gains.energy + "";
+        GameObject.Find("days").GetComponent<Text>().text = Profile.gains.days + "";
+        GameObject.Find("unlocking_trust").GetComponent<Text>().text = Profile.gains.unlocking_trust + "";
+    }
+}
