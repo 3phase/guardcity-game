8168b7b50bad808d5ae322e57d58e4f857812cb5
Nick Sot <nikola.s.sotirov@gmail.com>
2019-08-14 19:15:48 +0300

    Adequately displaying the dialog shits

--- Ombudsman-project/Assets/MissionController.cs
+++ Ombudsman-project/Assets/MissionController.cs
@@ -16,6 +16,8 @@ public class MissionController : MonoBehaviour
 
     public async void StartMission()
     {
+        gameObject.GetComponentInParent<Canvas>().enabled = false;
+
         Planet planet = await ApiController.GetPlanet(1);
 
         Node node = await ApiController.GetNode(41);
@@ -23,11 +25,25 @@ public class MissionController : MonoBehaviour
         loadNode(node);
     }
 
-    public void EndMission() {
+    public void EndMission(Node node) {
+        missionProblem = GameObject.Find("Problem").GetComponent<Text>();
+        missionProblem.text = node.dialog;
+
+        var option = Instantiate(optionPrefab, missionProblem.transform);
+        option.name = "End Mission";
+        option.transform.position = new Vector3(missionProblem.transform.position.x,
+                missionProblem.transform.position.y - 100, missionProblem.transform.position.z);
+        option.GetComponentInChildren<Text>().text = "Край на мисията";
+        option.GetComponent<Button>().onClick.AddListener(delegate { DestroyCanvas(); });
+    }
+
+    private void DestroyCanvas() {
         var canvas = GameObject.Find("MissionPanel").GetComponent<CanvasGroup>();
         canvas.alpha = 0;
         canvas.interactable = false;
         canvas.blocksRaycasts = false;
+
+        return;
     }
 
     public void ClearOptions() {
@@ -41,9 +57,11 @@ public class MissionController : MonoBehaviour
 
     public async void loadNode(Node node) {
         missionProblem = GameObject.Find("Problem").GetComponent<Text>();
-        missionProblem.text = node.dialog;
+        if (node == null) {
+            return;
+        }
 
-        Debug.Log(node.options);
+        missionProblem.text = node.dialog;
 
         //Profile.gains.popularity += node.gains.popularity;
         // Profile.gains.trust += node.gains.trust;
@@ -54,14 +72,22 @@ public class MissionController : MonoBehaviour
         ClearOptions();
 
         if (node.options.Count == 0) {
-            EndMission();
+            EndMission(node);
         } else {
             for (int i = 0; i < node.options.Count; i++)
             {
                 var option = Instantiate(optionPrefab, missionProblem.transform);
                 option.name = "Option" + i;
                 option.transform.position = new Vector3(missionProblem.transform.position.x, missionProblem.transform.position.y - (i + 1)*100, missionProblem.transform.position.z);
-                option.GetComponentInChildren<Text>().text = node.options[i].dialog;
+
+                if (node.options[i].speaker != "player" || node.options.Count == 1)
+                {
+                    option.GetComponentInChildren<Text>().text = "->";
+                }
+                else {
+                    option.GetComponentInChildren<Text>().text = node.options[i].dialog;
+                }
+
                 option.GetComponent<Info>().node = await ApiController.GetNode(node.options[i].id);
                 options.Add(option);
             }
