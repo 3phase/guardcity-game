4d51d828772d60f5f23b0d2c244468baf11cadfa
Krasimir Georgiev <krasi2405@gmail.com>
2019-11-11 22:54:24 +0200

    Asset Bundler

---
+++ AssetBundler/Assets/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AssetBundleBuilder : Editor
+{
+
+    [MenuItem("Assets/BuildBundle")]
+    static async void BuildBundle()
+    {
+        string bundleName = "";
+        List<string> assetPaths = new List<string>();
+
+        string[] args = System.Environment.GetCommandLineArgs();
+        for(int i = 1; i < args.Length; i++)
+        {
+            if(args[i] == "-bundleName")
+            {
+                if(i + 1 < args.Length)
+                {
+                    bundleName = args[i + 1];
+                }
+                else
+                {
+                    Debug.LogError("Cannot set bundle name since it is out of range!");
+                }
+            }
+            else if(args[i] == "-assetPaths")
+            {
+                for(int j = i + 1; j < args.Length && args[j][0] != '-'; j++)
+                {
+                    assetPaths.Add(args[j]);
+                }
+            }
+        }
+        
+
+        List<UnityWebRequestAsyncOperation> webRequests = new List<UnityWebRequestAsyncOperation>();
+        foreach (string assetPath in assetPaths)
+        {
+            UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture("file://" + assetPath);
+            UnityWebRequestAsyncOperation asyncOp = webRequest.SendWebRequest();
+            webRequests.Add(asyncOp);
+        }
+
+        List<Object> assets = new List<Object>();
+
+        List<string> assetNames = new List<string>();
+        for (int i = 0; i < webRequests.Count; i++) {
+            UnityWebRequestAsyncOperation asyncOp = webRequests[i];
+            
+            string filename = Path.GetFileNameWithoutExtension(asyncOp.webRequest.uri.AbsolutePath);
+
+            while (asyncOp.isDone == false) {
+                await Task.Delay(100);
+            }
+
+            Texture2D texture = DownloadHandlerTexture.GetContent(asyncOp.webRequest);
+
+            byte[] textureData = texture.EncodeToPNG();
+
+            string path = Application.dataPath + "/Resources/" + filename + ".png";
+            File.WriteAllBytes(path, textureData);
+
+            assetNames.Add("Assets/Resources/" + filename + ".png");
+        }
+
+
+        AssetBundleBuild bundleBuild = new AssetBundleBuild();
+        bundleBuild.assetBundleName = bundleName;
+        bundleBuild.assetNames = assetNames.ToArray();
+
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+        buildMap[0] = bundleBuild;
+
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles/", buildMap, BuildAssetBundleOptions.ForceRebuildAssetBundle, BuildTarget.WebGL);
+
+        EditorApplication.Exit(0);
+    }
+}
